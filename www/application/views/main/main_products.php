f4e29f20586c110ea001911a388d02d8145c7be0
BillJeong <printline83@naver.com>
2026-04-16 20:44:49 +0900

    style: 컨테이너 스타일 클래스 추가

--- www/application/views/main/main_products.php
+++ www/application/views/main/main_products.php
@@ -1,4 +1,4 @@
-<div class="container py-4 mx-auto" style="max-width: 600px;">
+<div class="container py-4 mx-auto max-w-600">
     <h3 class="mb-4 fw-bold text-center">상품 목록</h3>
     <div id="product-list" class="product-list"></div>
 </div>
--- www/application/views/main/main_reserve.php
+++ www/application/views/main/main_reserve.php
@@ -1,4 +1,4 @@
-<div class="container py-4 mx-auto" style="max-width: 600px;">
+<div class="container py-4 mx-auto max-w-600">
     <h3 class="mb-4 fw-bold text-center">상품 예약</h3>
     
     <div class="card border-0 shadow-sm">
